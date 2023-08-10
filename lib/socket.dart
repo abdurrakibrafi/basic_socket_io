0d99f6b1073226dd2fcf75768cfed0ad62cbe615
shafiul-haque-johny <mhaque171319@bscse.uiu.ac.bd>
2023-08-10 21:02:01 +0600

    4th

---
+++ lib/socket.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class SocketHome extends StatefulWidget {
+  const SocketHome({Key? key}) : super(key: key);
+
+  @override
+  State<SocketHome> createState() => _SocketHomeState();
+}
+
+class _SocketHomeState extends State<SocketHome> {
+  final TextEditingController my_controller = TextEditingController();
+  final my_channel =
+      WebSocketChannel.connect(Uri.parse('wss://echo.websocket.events'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Web Socket Demo',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.purple,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Form(
+                child: TextFormField(
+              controller: my_controller,
+              decoration: const InputDecoration(labelText: 'Send Message'),
+            )),
+            const SizedBox(
+              height: 24,
+            ),
+            StreamBuilder(
+                stream: my_channel.stream,
+                builder: (context, snapshot) {
+                  return Text(snapshot.hasData ? '${snapshot.data}' : '');
+                })
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: sendMessage,
+        tooltip: 'Send Message',
+        child: const Icon(Icons.send),
+      ),
+    );
+  }
+
+  // sending message
+  void sendMessage() {
+    if (my_controller.text.isNotEmpty) {
+      my_channel.sink.add(my_controller.text);
+      my_controller.text = '';
+    }
+  }
+
+  // close the connection
+  @override
+  void dispose() {
+    my_channel.sink.close();
+    my_controller.dispose();
+    super.dispose();
+  }
+}
